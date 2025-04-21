025620ce87fe2b02520d626a3ca68e681bbc7bd5
Suh <rlddesignerr@gmail.com>
2025-04-20 22:51:25 -0400

    ;.,kmjhgfdxsz

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,6 +1,53 @@
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: "Webhook funcionando! 🌼",
+const { createClient } = require('@supabase/supabase-js');
+const emailjs = require('@emailjs/nodejs');
+
+const supabase = createClient(
+  'https://tcnonaprqfcpqmscrbkg.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjbm9uYXBycWZjcHFtc2NyYmtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2MzYzOTQsImV4cCI6MjA2MDIxMjM5NH0.0yyj8uPdF3C3eQGPrFBWVvHuczCnHKhnXGsbBpN96Xw'
+  );
+
+  emailjs.init("avKNQkuVniC59Omdz");
+  exports.handler = async (event) => {
+    if (event.httpMethod !== 'POST') {
+      return { statusCode: 405, body: 'Método não permitido' };
+    }
+  
+    const { email, codigo } = JSON.parse(event.body || '{}');
+  
+    if (!email || !codigo) {
+      return { statusCode: 400, body: 'Email ou código ausente.' };
+    }
+  
+    const { data: capa, error } = await supabase
+      .from('preprontas')
+      .select('*')
+      .eq('codigo', codigo)
+      .single();
+  
+    if (error || !capa) {
+      return { statusCode: 404, body: 'Capa não encontrada.' };
+    }
+  
+    try {
+      await emailjs.send('service_vafq5zq', 'template_l3x34bo', {
+        email,
+        codigo: capa.codigo,
+        link_arquivos: capa.link_arquivos
+      });
+  
+      await supabase
+        .from('preprontas')
+        .update({ disponivel: false, reservada: false })
+        .eq('codigo', codigo);
+  
+      return {
+        statusCode: 200,
+        body: 'Enviado com sucesso.'
+      };
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: 'Erro ao enviar.'
+      };
+    }
   };
-};
