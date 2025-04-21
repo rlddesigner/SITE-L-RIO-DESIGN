e5d7fc558110358e54d6e739c9aba3df190a638b
Suh <rlddesignerr@gmail.com>
2025-04-20 21:26:12 -0400

    xdfvbghynh

--- netlify/functions/enviar-capa.js
+++ netlify/functions/enviar-capa.js
@@ -1,15 +1,12 @@
 const emailjs = require('@emailjs/nodejs');
 const { createClient } = require('@supabase/supabase-js');
 
+exports.handler = async (event, context) => {
 const client = supabase.createClient(
     'https://tcnonaprqfcpqmscrbkg.supabase.co',
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjbm9uYXBycWZjcHFtc2NyYmtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2MzYzOTQsImV4cCI6MjA2MDIxMjM5NH0.0yyj8uPdF3C3eQGPrFBWVvHuczCnHKhnXGsbBpN96Xw'
   );
-  emailjs.init("avKNQkuVniC59Omdz");
-
-exports.handler = async (event, context) => {
   const body = JSON.parse(event.body);
-
   const email = body.email;
   const codigo = body.codigo;
 
@@ -33,16 +30,13 @@ exports.handler = async (event, context) => {
     };
   }
 
-  const { link_arquivos } = data;
-
   try {
     await emailjs.send("service_vafq5zq", "template_l3x34bo", {
       email: email,
-      codigo: codigo,
-      link_arquivos: link_arquivos
+      codigo: data.codigo,
+      link_arquivos: data.link_arquivos
     });
 
-    // Marcar como vendida
     await supabase
       .from("preprontas")
       .update({ disponivel: false, reservada: false })
@@ -52,10 +46,11 @@ exports.handler = async (event, context) => {
       statusCode: 200,
       body: JSON.stringify({ status: "ok" })
     };
+
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Erro ao enviar o e-mail", detalhes: err })
+      body: JSON.stringify({ error: err.message })
     };
   }
 };
